47455d8adb75225a6666d2efb5471110342aa456
Andre <andredelima92@gmail.com>
2022-05-26 22:39:02 -0300

    Added styles in application

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,17 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 
 const Home: NextPage = () => {
-  return <h1>hello world</h1>;
+  return (
+    <>
+      <Head>
+        <title>Inicio | ig.news</title>
+      </Head>
+      <h1>
+        hello <span>world</span>
+      </h1>
+    </>
+  );
 };
 
 export default Home;
