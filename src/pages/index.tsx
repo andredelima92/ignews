ea4ccf1eec54535ad01973f7722e214cc5cb914e
Andre <andredelima92@gmail.com>
2022-07-12 20:41:42 -0300

    Home styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,28 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 
+import styles from "./home.module.scss";
+
 const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Inicio | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>
-        hello <span>world</span>
-      </h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>Hey, welcome 👏</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get Access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 };
